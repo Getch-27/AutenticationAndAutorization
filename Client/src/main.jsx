cdc462a69e0a0bd8603799bbbbaa8463f615b7c7
Getch <92431910+Getch-27@users.noreply.github.com>
2024-11-10 23:23:42 +0300

    front

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,7 +6,7 @@ import Signup from "./pages/Signup";
 import Profile from "./pages/Profile"; 
 import { TokenProvider } from "./context/TokenContext";
 import "./index.css";
-
+////
 const router = createBrowserRouter([
   {
     path: "/",
