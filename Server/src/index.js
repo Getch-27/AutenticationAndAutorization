62b927dde356b5c7ea220f3d35c45d319f79bbfb
Getch-27 <getahunlast@gmail.com>
2024-11-10 12:42:50 +0300

    feat : small fix

--- Server/src/index.js
+++ Server/src/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const routes = require("./routes");
 const cookieParser = require("cookie-parser");
 const databaseConnect = require("./database");
+require("dotenv").config(); // Load environment variables
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,20 +12,32 @@ async function startServer() {
   try {
     await databaseConnect(); // Database connection
 
-    app.use( // Handle CORS requests
-      cors({
-        origin: "http://localhost:5173",
-        methods: "GET,POST,PUT,DELETE,OPTIONS",
-        allowedHeaders: "Content-Type,Authorization",
-        credentials: true, 
-        httpOnly: true
-      })
-    );
+    const corsOptions = {
+      origin: process.env.CLIENT_URL || "http://localhost:5173", // Set client URL for CORS
+      methods: "GET,POST,PUT,DELETE,OPTIONS",
+      allowedHeaders: "Content-Type,Authorization",
+      credentials: true,
+      httpOnly: true,
+    };
 
+    app.use(cors(corsOptions));
     app.use(express.json());
     app.use(cookieParser());
     app.use("/api", routes);
 
+    app.get("/", (req, res) => {
+      const serverUrl = process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : `http://localhost:${port}`;
+
+      res.json({
+        message: "Server is running",
+        serverLink: serverUrl,
+        databaseStatus: "Connected", // Assuming databaseConnect() does not throw an error if successful
+      });
+    });
+
+    // Error handling middleware
     app.use((err, req, res, next) => {
       console.error(err);
       res.status(err.status || 500).send({ error: err.message || "Internal Server Error" });
